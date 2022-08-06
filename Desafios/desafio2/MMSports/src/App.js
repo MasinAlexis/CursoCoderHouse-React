0b345ae89c11e573d8cc285b76660bd25e408446
MasinAlexis <AlexisMasin86@gmail.com>
2022-08-06 17:20:54 -0300

    Desafio 2 completo

---
+++ Desafios/desafio2/MMSports/src/App.js
@@ -0,0 +1,12 @@
+import NavBar from './components/navbar/NavBar';
+import './App.css'
+
+function App() {
+  return (
+      <div className='container'>
+        <NavBar/>
+      </div>
+    );
+}
+
+export default App;
